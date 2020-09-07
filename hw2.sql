2657e8cdbcd637d47c4b45114674f60ba4fc64b1
johnm1990 <68256475+johnm1990@users.noreply.github.com>
2020-09-06 20:49:04 -0400

    Add files via upload

---
+++ hw2.sql
@@ -0,0 +1,44 @@
+create table movieinfo(
+	reviewerid int not null,
+	firstname char(50) not null,
+    moviename varchar(50) not null,
+	movieid varchar(50)  not null,
+	rating varchar(5) null
+);
+SELECT * FROM movieinfo;
+
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Mulan',1,5);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Invasion',2,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Artemis',3,3);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Capone',4,2);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Tesla',5,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Tigertail',6,1);
+
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (2,'Lisa', 'Mulan',1,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (2,'Lisa', 'Invasion',2,3);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (2,'Lisa', 'Artemis',3,2);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (2,'Lisa', 'Capone',4,5);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (2,'Lisa', 'Tesla',5,1);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (2,'Lisa', 'Tigertail',6,2);
+
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (3,'Homer', 'Mulan',1,5);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (3,'Homer', 'Invasion',2,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (3,'Homer', 'Artemis',3,3);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (3,'Homer', 'Capone',4,2);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (3,'Homer', 'Tesla',5,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (3,'Homer', 'Tigertail',6,1);
+
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Mulan',1,5);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Invasion',2,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Artemis',3,3);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Capone',4,2);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Tesla',5,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Tigertail',6,1);
+
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Mulan',1,5);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Invasion',2,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Artemis',3,3);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Capone',4,2);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Tesla',5,4);
+INSERT INTO movieinfo (reviewerid, firstname, moviename, movieid, rating) VALUES (1,'Bart', 'Tigertail',6,1);
+
